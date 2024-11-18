697dac9a0339a8270b7c354c9d071b753151c22e
achamas811 <alexandra.chamas@stonybrook.edu>
2024-11-18 14:24:26 -0500

    EmpoweringDataCreation

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-  public static void main(String[] args){
-    System.out.println("Hello World");
-    System.out.println("Welcome to my GitHub!");
-    System.out.println("Feel free to reach out!");
-  }
-}
