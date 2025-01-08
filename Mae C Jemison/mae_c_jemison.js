010583583aec6c5af4ac2697c46fc82334df3b7c
achamas811 <alexandra.chamas@stonybrook.edu>
2025-01-07 21:47:15 -0500

    p5js

--- Mae C Jemison/mae_c_jemison.js
+++
@@ -1,4 +0,0 @@
-var background, constant, speed, y_axis, x_axis, score, coordinates, width, height, astronaut, challenger, cornell, doctor, maeJemison, nasa, peaceCorps, sallyRide, stanfordUniversity, racism, gender, doubleStandards, rejection, instructions;
-function stage(){
-  
-}
